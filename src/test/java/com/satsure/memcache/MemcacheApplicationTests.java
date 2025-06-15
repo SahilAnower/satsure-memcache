9659c6bbd58d1c029e4e2740293320254a606906
SahilAnower <sahilanower2@gmail.com>
2025-06-16 00:37:33 +0530

    Project setup

---
+++ src/test/java/com/satsure/memcache/MemcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satsure.memcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemcacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
